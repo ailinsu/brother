9b115a96920415845b36b6a5c055f4753d7a3bff
Ai Lin Su <ailinsu@hivestack.com>
2021-12-21 21:16:26 -0500

    center ze thing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <!-- <router-view/> -->
     <Clue/>
   </div>
@@ -28,18 +24,15 @@ export default {
   text-align: center;
   margin: auto;
   color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html,body {
+  height: 100%;
+  margin: 0;
 }
 </style>
