166e2a00ef94c4f89a1ab1b85d170adb1a849bb5
Connor Dovan <cdovan16@gmail.com>
2021-12-21 21:09:15 -0500

    clue

---
+++ src/components/Clue.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="clue">
+    <div class="phase">{{phase}}</div>
+    <div class="number">{{number}}</div>
+    <div class="text">aaaaaaaaaa{{text}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    phase: String,
+    number: Number,
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+.clue {
+  margin: auto;
+  display: block;
+  background-color: white;
+  border-top-color: black;
+  border-top-width: 5px;
+  border-bottom-color: black;
+  border-bottom-width: 5px;
+  width: 300px;
+  height: 200px;
+  filter: drop-shadow(0 0 10px rgb(150, 150, 150));
+  margin: 40px 0 0;
+}
+</style>
